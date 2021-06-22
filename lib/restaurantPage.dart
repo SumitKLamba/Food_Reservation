65d7cbc1a51be08e4dabc811876b5f4a566cd911
SumitKLamba <sumitskl1999@gmail.com>
2021-06-22 16:16:10 +0530

    time slots added

--- lib/restaurantPage.dart
+++ lib/restaurantPage.dart
@@ -11,115 +11,203 @@ class RestaurantPage extends StatefulWidget {
 }
 
 class _RestaurantPageState extends State<RestaurantPage> {
+  List<String> timeSlots = <String>[
+    "6:30 PM",
+    "6:45 PM",
+    "7:00 PM",
+    "7:15 PM",
+    "7:30 PM"
+  ];
+
   @override
   Widget build(BuildContext context) {
     Sizing.init(context);
     return SafeArea(
         child: Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            width: 1.sw,
-            height: 0.35.sh,
-            child: Image.asset('assets/${widget.restaurant["name"]}.jpg',fit: BoxFit.fill,),
+            body: Stack(
+      children: [
+        Container(
+          width: 1.sw,
+          height: 0.35.sh,
+          child: Image.asset(
+            'assets/${widget.restaurant["name"]}.jpg',
+            fit: BoxFit.fill,
           ),
-          Container(
-            height: 1.sh,
-            width: 1.sw,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Expanded(child: SizedBox(),),
-                Container(
-                  height: 0.68.sh,
-                  width: 1.sw,
-                  decoration: BoxDecoration(
+        ),
+        Container(
+          height: 1.sh,
+          width: 1.sw,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Expanded(
+                child: SizedBox(),
+              ),
+              Container(
+                height: 0.68.sh,
+                width: 1.sw,
+                decoration: BoxDecoration(
                     color: Colors.white,
-                    borderRadius: BorderRadius.only(topLeft: Radius.circular(50.sp), topRight: Radius.circular(50.sp))
-                  ),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.all(10.sp),
-                        child: Text(widget.restaurant["name"],
-                          style: GoogleFonts.openSans(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(50.sp),
+                        topRight: Radius.circular(50.sp))),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(10.sp),
+                      child: Text(
+                        widget.restaurant["name"],
+                        style: GoogleFonts.openSans(
                             textStyle: TextStyle(
-                              fontSize: 24.sp,
-                              fontWeight: FontWeight.bold,
-                              color: Color(0xFFf0932b)
-                            )
-                          ),
-                        ),
+                                fontSize: 24.sp,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xFFf0932b))),
                       ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 20.sp),
-                        child: Text(widget.restaurant["description"],
-                          style: GoogleFonts.openSans(
-                              textStyle: TextStyle(
+                    ),
+                    SingleChildScrollView(
+                      physics: BouncingScrollPhysics(),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 20.sp),
+                            child: Text(
+                              widget.restaurant["description"],
+                              style: GoogleFonts.openSans(
+                                  textStyle: TextStyle(
                                 fontSize: 14.sp,
                                 fontWeight: FontWeight.w500,
-                              )
+                              )),
+                            ),
                           ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(20.sp),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Row(
+                          Padding(
+                            padding: EdgeInsets.all(20.sp),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                               children: [
-                                Icon(Icons.location_on_outlined, color: Colors.blue, size: 24.sp ),
-                                SizedBox(width: 5.sp,),
-                                Text(widget.restaurant["distance"],
-                                  style: GoogleFonts.roboto(
-                                    textStyle: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 16.sp
+                                Row(
+                                  children: [
+                                    Icon(Icons.location_on_outlined,
+                                        color: Colors.blue, size: 24.sp),
+                                    SizedBox(
+                                      width: 5.sp,
+                                    ),
+                                    Text(
+                                      widget.restaurant["distance"],
+                                      style: GoogleFonts.roboto(
+                                          textStyle: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16.sp)),
                                     )
-                                  ),
-                                )
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    Icon(Icons.star_border_outlined,
+                                        color: Colors.yellow, size: 24.sp),
+                                    SizedBox(
+                                      width: 5.sp,
+                                    ),
+                                    Text(
+                                      widget.restaurant["rating"],
+                                      style: GoogleFonts.roboto(
+                                          textStyle: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16.sp)),
+                                    )
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    Icon(Icons.restaurant,
+                                        color: Colors.pink, size: 24.sp),
+                                    SizedBox(
+                                      width: 5.sp,
+                                    ),
+                                    Text(
+                                      widget.restaurant["type"],
+                                      style: GoogleFonts.roboto(
+                                          textStyle: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16.sp)),
+                                    )
+                                  ],
+                                ),
                               ],
                             ),
-                            Row(
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 20.sp),
+                            child: Row(
                               children: [
-                                Icon(Icons.star_border_outlined, color: Colors.yellow, size: 24.sp ),
-                                SizedBox(width: 5.sp,),
-                                Text(widget.restaurant["rating"],
+                                Icon(Icons.schedule),
+                                SizedBox(width: 7.sp,),
+                                Text(
+                                  'Available Time Slots',
                                   style: GoogleFonts.roboto(
                                       textStyle: TextStyle(
                                           fontWeight: FontWeight.bold,
-                                          fontSize: 16.sp
-                                      )
-                                  ),
-                                )
+                                          fontSize: 16.sp)),
+                                ),
                               ],
                             ),
-                            Row(
+                          ),
+                          Container(
+                            height: 0.08.sh,
+                            width: 1.sw,
+                            child: ListView.builder(
+                                physics: BouncingScrollPhysics(),
+                                scrollDirection: Axis.horizontal,
+                                itemCount: timeSlots.length,
+                                itemBuilder: (BuildContext context, int index) {
+                                  return Padding(
+                                    padding: EdgeInsets.symmetric(
+                                        vertical: 10.sp, horizontal: 7.sp),
+                                    child: Container(
+                                        width: 0.25.sw,
+                                        decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(12.sp),
+                                            color: Color(0xFFf0932b)
+                                                .withOpacity(0.8)),
+                                        child: Center(
+                                            child: Text(
+                                          timeSlots[index],
+                                          style: GoogleFonts.roboto(
+                                              textStyle: TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 14.sp,
+                                                  fontWeight: FontWeight.bold)),
+                                        ))),
+                                  );
+                                }),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 20.sp,vertical: 5.sp),
+                            child: Row(
                               children: [
-                                Icon(Icons.restaurant, color: Colors.pink, size: 24.sp ),
-                                SizedBox(width: 5.sp,),
-                                Text(widget.restaurant["type"],
+                                Icon(Icons.restaurant_menu),
+                                SizedBox(width: 7.sp,),
+                                Text(
+                                  'Menu',
                                   style: GoogleFonts.roboto(
                                       textStyle: TextStyle(
                                           fontWeight: FontWeight.bold,
-                                          fontSize: 16.sp
-                                      )
-                                  ),
-                                )
+                                          fontSize: 16.sp)),
+                                ),
                               ],
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                )
-              ],
-            ),
+                    )
+                  ],
+                ),
+              )
+            ],
           ),
-        ],
-      )
-    ));
+        ),
+      ],
+    )));
   }
 }
